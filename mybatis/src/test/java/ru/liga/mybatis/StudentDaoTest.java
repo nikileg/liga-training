15992399730440ec59679c19313a7452e23a8858
nikileg <10009432+nikileg@users.noreply.github.com>
2018-04-23 15:22:54 +0300

    More tests for StudentDao.
    MyBatis-generated daos' inserts and updates return either int (#of rows affected) or void,
     so I changed method signatures in interfaces accordingly.

--- mybatis/src/test/java/ru/liga/mybatis/StudentDaoTest.java
+++ mybatis/src/test/java/ru/liga/mybatis/StudentDaoTest.java
@@ -1,5 +1,6 @@
 package ru.liga.mybatis;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +8,26 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.liga.mybatis.config.TestDaoSpringConfig;
 import ru.liga.mybatis.dao.StudentDao;
+import ru.liga.mybatis.entity.StudentEntity;
 
-import static org.junit.Assert.assertEquals;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {TestDaoSpringConfig.class})
 public class StudentDaoTest {
+    List<Long> insert_ids = new ArrayList<>();
 
     @Autowired
     private StudentDao studentDao;
+
+    @After
+    public void cleanInserted() {
+        insert_ids.forEach(id -> studentDao.delete(id));
+    }
 
     @Test
     public void selectById() {
@@ -25,6 +37,49 @@ public class StudentDaoTest {
     @Test
     public void selectByDepartmentId() {
         assertEquals(16, studentDao.selectByDepartmentId(1L).size());
+    }
+
+    @Test
+    public void selectNonExistingId() {
+        long ne_id = -1;
+        StudentEntity null_student = studentDao.select(ne_id);
+        assertNull(null_student);
+    }
+
+    @Test
+    public void insert() {
+        StudentEntity toInsert = new StudentEntity(
+                null,
+                "test fio",
+                "APACHE ATTACK HELICOPTER",
+                2L,
+                1L,
+                LocalDate.now());
+        studentDao.insert(toInsert);
+        long id = toInsert.getId();
+        assertNotNull(id);
+        StudentEntity afterInsert = studentDao.select(id);
+        assertEquals(toInsert, afterInsert);
+        this.insert_ids.add(id);
+    }
+
+    @Test
+    public void insertAndthenUpdate() {
+        final String updatedFio = "fiofiofiofio";
+        StudentEntity toInsert = new StudentEntity(
+                null,
+                "test fio",
+                "APACHE ATTACK HELICOPTER",
+                2L,
+                1L,
+                LocalDate.now());
+        studentDao.insert(toInsert);
+        long id = toInsert.getId();
+        toInsert.setFio(updatedFio);
+        studentDao.update(toInsert);
+        StudentEntity afterUpdate = studentDao.select(id);
+        assertEquals(toInsert, afterUpdate);
+        this.insert_ids.add(id);
     }
 
 }
